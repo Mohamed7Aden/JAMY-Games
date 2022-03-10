2e83716c722ed1735ab26f8a9e28170461ec7c55
SleepyJake18 <94761866+SleepyJake18@users.noreply.github.com>
2022-03-10 14:49:02 -0500

    merging latest changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const axios = require(`axios`).default;
-
-// Prevent non logged in users from viewing the homepage
-=======
 const { User } = require('../models');
 const axios = require('axios').default;
 const withAuth = require('../utils/auth');
@@ -11,7 +6,6 @@ const withAuth = require('../utils/auth');
 
 
 
->>>>>>> 2b9d83a4dae8e8e1e8903600a9ff535bc901c6ec
 router.get('/', async (req, res) => {
   try {
     const allGameData = {
@@ -24,19 +18,11 @@ router.get('/', async (req, res) => {
     };
     axios.request(allGameData).then(function (response) {
       console.log(response.data);
-<<<<<<< HEAD
-    }).catch(function (error) {
-      console.error(error);
-    });
-
-    res.render('homepage', {
-=======
       res.render('homepage', {
         games: response.data
       });
     }).catch(function (error) {
       console.error(error);
->>>>>>> 2b9d83a4dae8e8e1e8903600a9ff535bc901c6ec
     });
   } catch (err) {
     res.status(500).json(err);
