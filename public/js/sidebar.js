4142313ca634e0086b6c35cded99de2deec4d8bb
Yazid <yalhujery01@gmail.com>
2022-03-11 14:51:23 -0500

    created partial to render each game

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
       // specify options here
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      // specify options here
+    });
+  });
