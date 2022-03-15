f230dc3d9a30a99beb48bc46b2d9769c11e4d73e
SleepyJake18 <94761866+SleepyJake18@users.noreply.github.com>
2022-03-15 10:37:43 -0400

    coded in temporary solution for dashboard page for demonstration

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,26 +1,35 @@
 const router = require('express').Router();
 const { User, SavedGames, Game_details } = require('../models');
 const withAuth = require('../utils/auth');
+const axios = require("axios").default;
 
 
 // GET all games for dashboard
-router.get('/', withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-      let games = {};
-      const userGameData = await User.findByPk(req.session.user_id, {
-        include: [{model: Game_details, through: SavedGames, as: 'my_games'}]
-      });
-      console.log(userGameData);
-       games = userGameData.get({plain:true});
-       console.log(games);
-      res.render("dashboard", {
-        userGames: games
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    const allGameData = {
+      method: "GET",
+      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      headers: {
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+        "x-rapidapi-key": "1a3958a999msh626d8d771ef20b0p1e89b7jsn52537a78b46c",
+      },
+    };
+  
+    axios.request(allGameData).then(function (response) {
+     
+          res.render("dashboard", {
+            games: response.data,
+          });
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
     }
-  });
+   catch (err) {
+    res.status(500).json(err);
+  }
+});
 
   // GET one game
 router.get('/game/:id', withAuth, async (req, res) => {
